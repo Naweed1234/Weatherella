33c075def8570d21c34c0b432d7416cd903f62e9
franklinamaya <franklinamaya>
2022-09-11 17:04:50 -0400

    first commit

---
+++ weatherella/src/App.js
@@ -0,0 +1,12 @@
+import './App.css';
+
+function App() {
+  return (
+  <div>
+    <input type="search" className='w-full h-full border-3 border-red'>
+    </input>
+  </div>
+  );
+}
+
+export default App;
